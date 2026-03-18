c62845498d8ed63a3dcf25c88dfdcef76f0a41d5
Sam Moore <samhmoore95@gmail.com>
2026-03-18 11:50:15 -0400

    Add exploration notebook and update pipeline script to include data inspection step

--- src/pipeline_practice_2.py
+++ src/pipeline_practice_2.py
@@ -1,1 +1,7 @@
 # Step 1: Load CSV with Pandas
+from tabulate import tabulate
+import pandas as pd
+
+job_data = pd.read_csv("data/jobs_practice_2.csv")
+
+# Step 2: Inspect Data in Jupyter Notebook
